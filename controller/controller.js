eba7709b5c573a4cba6060c9523cd467909b9ed2
David Leigh <daleighan@gmail.com>
2018-04-22 21:25:04 -0700

    Refactored to have a controller

---
+++ controller/controller.js
@@ -0,0 +1,82 @@
+const jwt = require('jsonwebtoken');
+const PROJECTS_TABLE = process.env.PROJECTS_TABLE;
+const IS_OFFLINE = process.env.IS_OFFLINE;
+const secret = process.env.SECRET || 'shhhhhhhhh';
+const AWS = require('aws-sdk');
+
+let dynamoDb;
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+  });
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+}
+
+const root = (req, res) => {
+  res.json("Welcome to Alex's API");
+};
+
+const login = (req, res) => {
+  const {username, password} = req.body;
+  if (username === 'alexleigh' && password === 'password') {
+    const params = {
+      admin: username,
+      auth: 'magic',
+      agent: req.headers['user-agent'],
+      exp: Math.floor(new Date().getTime() / 1000) + 7 * 24 * 60 * 60,
+    };
+    const token = jwt.sign(params, secret);
+    res.cookie('jwtToken', token, {maxAge: 900000, httpOnly: true});
+    res.send('here is your cookie');
+  } else {
+    res.json('invalid credentials');
+  }
+};
+
+const getProjects = (req, res) => {
+  const params = {
+    TableName: PROJECTS_TABLE,
+  };
+  dynamoDb.scan(params, (error, result) => {
+    if (error) {
+      res.status(400).json({error});
+    }
+    res.json(result);
+  });
+};
+
+const addProject = (req, res) => {
+  const params = {
+    TableName: PROJECTS_TABLE,
+    Item: {
+      ...req.body,
+      pictures: [],
+    },
+  };
+  dynamoDb.put(params, error => {
+    if (error) {
+      res.status(400).json({error});
+    }
+    res.json(params.Item);
+  });
+}
+
+const deleteProject = (req, res) => {
+  const {projectName} = req.body;
+  const params = {
+    TableName: PROJECTS_TABLE,
+    Key: {
+      projectName,
+    },
+  };
+  dynamoDb.delete(params, error => {
+    if (error) {
+      res.status(400).json({error});
+    }
+    res.json(`Project deleted: ${projectName}`);
+  });
+}
+
+module.exports = {root, login, getProjects, addProject, deleteProject};
--- router/router.js
+++ router/router.js
@@ -1,19 +1,8 @@
 const router = require('express').Router();
-const secret = process.env.SECRET || 'shhhhhh';
-const AWS = require('aws-sdk');
 const jwt = require('jsonwebtoken');
-const PROJECTS_TABLE = process.env.PROJECTS_TABLE;
-const IS_OFFLINE = process.env.IS_OFFLINE;
-
-let dynamoDb;
-if (IS_OFFLINE === 'true') {
-  dynamoDb = new AWS.DynamoDB.DocumentClient({
-    region: 'localhost',
-    endpoint: 'http://localhost:8000',
-  });
-} else {
-  dynamoDb = new AWS.DynamoDB.DocumentClient();
-}
+const controller = require('../controller/controller');
+const {root, login, getProjects, addProject, deleteProject} = controller;
+const secret = process.env.SECRET || 'shhhhhhhhh';
 
 const verifyJWT = (req, res, next) => {
   const token = req.cookies.jwtToken;
@@ -31,69 +20,10 @@ const verifyJWT = (req, res, next) => {
   }
 };
 
-router.get('/', (req, res) => {
-  res.send('Welcome to Alex Leigh\'s page');
-});
-
-router.post('/login', (req, res) => {
-  const {username, password} = req.body;
-  if (username === 'alexleigh' && password === 'password') {
-    const params = {
-      admin: username,
-      auth:  'magic',
-      agent: req.headers['user-agent'],
-      exp:   Math.floor(new Date().getTime()/1000) + 7*24*60*60,
-    }
-    const token = jwt.sign(params, secret);
-    res.cookie('jwtToken', token, {maxAge: 900000, httpOnly: true});
-    res.send('here is your cookie');
-  } else {
-    res.json('invalid credentials');
-  }
-});
-
-router.get('/projects', verifyJWT, (req, res) => {
-  const params = {
-    TableName: PROJECTS_TABLE,
-  };
-  dynamoDb.scan(params, (error, result) => {
-    if (error) {
-      res.status(400).json({error});
-    }
-    res.json(result);
-  });
-});
-
-router.post('/projects', verifyJWT, (req, res) => {
-  const params = {
-    TableName: PROJECTS_TABLE,
-    Item: {
-      ...req.body,
-      pictures: [],
-    },
-  };
-  dynamoDb.put(params, error => {
-    if (error) {
-      res.status(400).json({error});
-    }
-    res.json(params.Item);
-  });
-});
-
-router.delete('/projects', verifyJWT, (req, res) => {
-  const {projectName} = req.body;
-  const params = {
-    TableName: PROJECTS_TABLE,
-    Key: {
-      projectName,
-    },
-  };
-  dynamoDb.delete(params, error => {
-    if (error) {
-      res.status(400).json({error});
-    }
-    res.json(`Project deleted: ${projectName}`);
-  });
-});
+router.get('/', root);
+router.post('/login', login);
+router.get('/projects', verifyJWT, getProjects);
+router.post('/projects', verifyJWT, addProject);
+router.delete('/projects', verifyJWT, deleteProject);
 
 module.exports = router;
